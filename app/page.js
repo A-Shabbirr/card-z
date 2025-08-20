bccf52261bd9235336ea67aebc3811d4aecd5c0d
Adnan <A.A.Shabbirr96@gmail.com>
2025-08-20 13:41:01 -0700

    Responsive changes

--- app/page.js
+++ app/page.js
@@ -2,12 +2,13 @@ import Card from "@/Card/page";
 
 export default function Home() {
   return (
-    <div className="bg-red-400 max-w-screen w-full h-screen flex flex-col xl:flex-row justify-center items-center">
+    <div className="bg-red-400 w-full min-h-screen flex flex-col xl:flex-row justify-center items-center overflow-auto p-4">
       <Card />
       <Card />
       <Card />
       <Card />
     </div>
+
 
   );
 }
